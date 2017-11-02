8122b4f02e2bb56e0cc85ddb51240b9a5a9809f7
yukai <yukai@zbj.com>
2017-11-02 10:30:38 +0800

    :smile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
 	<div id="app">
+		<Header></Header>
 		<router-view/>
 	</div>
 </template>
 
 <script>
+import Header from '@/components/common/Header.vue';
 export default {
-	name: 'app'
+	name: 'app',
+	components: {
+		Header
+	},
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	}
 }
 </script>
